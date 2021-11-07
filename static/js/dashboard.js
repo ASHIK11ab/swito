a3bb577ac421117ed9acb139593b1ff2b7efa213
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2021-11-07 12:00:12 +0530

    added mobile navbar styles and functionality

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,5 +1,7 @@
 const sidebar = document.getElementById("app-sidebar");
 const sidebar_toggle_btn = document.getElementById("sidebar-toggle-btn");
+const mobile_sidebar_toggle_btn = document.getElementById(
+                                    "mobile-sidebar-toggle-btn");
 const sidebar_close_btn = document.getElementById("sidebar-close-btn");
 let modal_bg = undefined;
 
@@ -17,14 +19,19 @@ function remove_modal_bg() {
 }
 
 function toggle_sidebar() {
-  create_modal_bg();
-  sidebar.classList.add("toggle-sidebar-left");
+  /* Prevents multiple modals bg's being creating when
+    the toggle btn is clicked continuosly */
+  if(!sidebar.classList.contains('toggle-sidebar')) {
+    create_modal_bg();
+    sidebar.classList.add("toggle-sidebar");
+  }
 }
 
 function close_sidebar() {
   remove_modal_bg();
-  sidebar.classList.remove("toggle-sidebar-left");
+  sidebar.classList.remove("toggle-sidebar");
 }
 
 sidebar_toggle_btn.addEventListener("click", toggle_sidebar);
+mobile_sidebar_toggle_btn.addEventListener("click", toggle_sidebar);
 sidebar_close_btn.addEventListener("click", close_sidebar);
