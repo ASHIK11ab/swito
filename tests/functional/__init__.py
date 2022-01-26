6a366535e5c032470de36fa1792466727231f7cf
ASHIK11ab <ashikmeeranmohideen@gmail.com>
2022-01-26 21:52:42 +0530

    updated __init__.py

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -4,3 +4,7 @@
 
   Contains all of the integration tests for the application.
 """
+from app import app
+
+ADMIN_USERNAME = app.config['TEST_ADMIN_USERNAME']
+ADMIN_PASSWORD = app.config['TEST_ADMIN_PASSWORD']
