9fb87c235b7da6818d9c639586e8ebd233d28049
ASHIK11ab <ashikmeeranmohideen@gmail.com>
2022-02-12 10:42:11 +0530

    moved foods section code to dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -2,6 +2,7 @@ const sidebar = document.getElementById("app-sidebar");
 const sidebar_toggle_btn = document.getElementById("sidebar-toggle-btn");
 const mobile_sidebar_toggle_btn = document.getElementById("mobile-sidebar-toggle-btn");
 const sidebar_close_btn = document.getElementById("sidebar-close-btn");
+const search_form = document.getElementById("search-form")
 let modal_bg = undefined;
 
 
@@ -29,6 +30,55 @@ function toggle_component(component) {
 function close_component(component) {
   remove_modal_bg();
   component.classList.remove('toggle');
+}
+
+
+// Creates the food section using the foods data.
+function create_foods_section(foods) {
+  const section = document.createElement('section');
+  section.setAttribute('class', 'foods-section');
+  section.innerHTML += 
+  `<h2 class="section-title">Recommended For You</h2>`;
+
+  let foods_container = "";
+  // Dynamically creates list of food cards.
+  for(const food of foods) {
+    foods_container +=
+    `<div class="food-card" data-food-id="${food.id}">` +
+      `<div class="food-image">` +
+        `<img src="${food.img_url}" alt="${food.name}" image - Swito">` +
+      `</div>` +
+      `<div class="card-body">` +
+        `<span class="food-name">${food.name}</span>` +
+        `<span class="food-tag">${food.tag}</span>` +
+        `<span class="food-price">${food.price}</span>` +
+      `</div>` +
+    `</div>`;
+  }
+
+  section.innerHTML += `<div class="foods">${foods_container}</div>`;
+  return section
+}
+
+
+search_form.onsubmit = function() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/dashboard', true)
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      const resp = JSON.parse(this.responseText);
+      const main_container = document.querySelector('.main-container');
+      const section = create_foods_section(resp);
+      main_container.appendChild(section)
+    }
+  }
+
+  let formData = new FormData();
+  const query = document.getElementById("query").value;
+  formData.append("query", query);
+
+  xhr.send(formData);
+  return false;
 }
 
 
--- static/js/food.js
+++ static/js/food.js
@@ -1,31 +1,3 @@
-// Creates the food section using the foods data.
-function create_foods_section(foods) {
-  const section = document.createElement('section');
-  section.setAttribute('class', 'foods-section');
-  section.innerHTML += 
-  `<h2 class="section-title">Recommended For You</h2>`;
-
-  let foods_container = "";
-  // Dynamically creates list of food cards.
-  for(const food of foods) {
-    foods_container +=
-    `<div class="food-card" data-food-id="${food.id}">` +
-      `<div class="food-image">` +
-        `<img src="${food.img_url}" alt="${food.name}" image - Swito">` +
-      `</div>` +
-      `<div class="card-body">` +
-        `<span class="food-name">${food.name}</span>` +
-        `<span class="food-tag">${food.tag}</span>` +
-        `<span class="food-price">${food.price}</span>` +
-      `</div>` +
-    `</div>`;
-  }
-
-  section.innerHTML += `<div class="foods">${foods_container}</div>`;
-  return section
-}
-
-
 window.addEventListener('DOMContentLoaded', () => {
   // Get the similar foods asynchronously using AJAX.
   const xhr = new XMLHttpRequest();
